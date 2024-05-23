fe9dc5605966b44d60616c0e6c8b2357605f959c
franc <franc@BLUE>
2024-05-22 19:19:28 -0700

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,107 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtNumber = New System.Windows.Forms.TextBox()
+        Me.btnRecord = New System.Windows.Forms.Button()
+        Me.btnAverage = New System.Windows.Forms.Button()
+        Me.txtAverage = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(43, 73)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(47, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Number:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(123, 193)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(50, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Average:"
+        '
+        'txtNumber
+        '
+        Me.txtNumber.Location = New System.Drawing.Point(109, 70)
+        Me.txtNumber.Name = "txtNumber"
+        Me.txtNumber.Size = New System.Drawing.Size(100, 20)
+        Me.txtNumber.TabIndex = 2
+        '
+        'btnRecord
+        '
+        Me.btnRecord.Location = New System.Drawing.Point(229, 73)
+        Me.btnRecord.Name = "btnRecord"
+        Me.btnRecord.Size = New System.Drawing.Size(141, 23)
+        Me.btnRecord.TabIndex = 3
+        Me.btnRecord.Text = "Record Number"
+        Me.btnRecord.UseVisualStyleBackColor = True
+        '
+        'btnAverage
+        '
+        Me.btnAverage.Location = New System.Drawing.Point(109, 128)
+        Me.btnAverage.Name = "btnAverage"
+        Me.btnAverage.Size = New System.Drawing.Size(261, 23)
+        Me.btnAverage.TabIndex = 4
+        Me.btnAverage.Text = "Calculate Average"
+        Me.btnAverage.UseVisualStyleBackColor = True
+        '
+        'txtAverage
+        '
+        Me.txtAverage.Location = New System.Drawing.Point(210, 193)
+        Me.txtAverage.Name = "txtAverage"
+        Me.txtAverage.Size = New System.Drawing.Size(100, 20)
+        Me.txtAverage.TabIndex = 5
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(488, 285)
+        Me.Controls.Add(Me.txtAverage)
+        Me.Controls.Add(Me.btnAverage)
+        Me.Controls.Add(Me.btnRecord)
+        Me.Controls.Add(Me.txtNumber)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "AVERAGE"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtNumber As TextBox
+    Friend WithEvents btnRecord As Button
+    Friend WithEvents btnAverage As Button
+    Friend WithEvents txtAverage As TextBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,37 @@
+﻿
+Public Class Form1
+    Dim stats As New Statistics()  ''creates new statistics object instance
+
+    Private Sub btnRecord_Click(sender As Object, e As EventArgs) Handles btnRecord.Click
+        ''add parse number to statistics array
+        stats.AddNumber(Double.Parse(txtNumber.Text))
+        txtNumber.Clear()  ''clears input text box
+    End Sub
+
+    Private Sub btnAverage_Click(sender As Object, e As EventArgs) Handles btnAverage.Click
+        ''calculates average using Statistics class method
+        Dim average As Double = stats.Average()
+        ''set average value to text
+        txtAverage.Text = average.ToString()
+    End Sub
+End Class
+
+Public Class Statistics
+    Private numbers(49) As Double  ''array hold up to 50 double values
+    Private count As Integer       ''variable tracks count of numbers
+
+    Public Sub AddNumber(ByVal number As Double)
+        ''adds number to array, increments count
+        numbers(count) = number
+        count += 1
+    End Sub
+
+    Public Function Average() As Double
+        ''calcualates sum, divides by count
+        Dim sum As Double = 0
+        For i As Integer = 0 To count - 1
+            sum += numbers(i)
+        Next
+        Return sum / count
+    End Function
+End Class
